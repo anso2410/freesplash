50e4ecd978ced82301e1597e829fb67f3b6df9ad
anso2410 <simana.annesophie@gmail.com>
2022-04-30 16:27:03 +0200

    web.php:create route for testing verified email with adding middleware auth and verified & User.php: implementation of  the use MustVerificationemail

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use GuzzleHttp\Middleware;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('test', function(){
+    return 'test';
+})->middleware(['auth', 'verified']);
 
 Route::get('/', function () {
     return view('welcome');
