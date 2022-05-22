dc99cc3acda9df0c95724648af89fc3c584894c7
anso2410 <simana.annesophie@gmail.com>
2022-05-22 16:38:14 +0200

    set relation with photo for source table

--- app/Models/Source.php
+++ app/Models/Source.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Source extends Model
 {
     use HasFactory;
+
+    public function photo()
+    {
+        return $this->belongsTo(Photo::class);
+    }
 }
