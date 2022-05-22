9e7a791923da1cfc5628e19bd02914e3a5e20257
anso2410 <simana.annesophie@gmail.com>
2022-05-22 17:47:43 +0200

    PhotoFactory:set up rules for faker

--- database/factories/PhotoFactory.php
+++ database/factories/PhotoFactory.php
@@ -2,10 +2,20 @@
 
 namespace Database\Factories;
 
+use App\Models\Photo;
+use App\Models\Album;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Http\File;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class PhotoFactory extends Factory
 {
+      /**
+     * @var string
+     */
+    protected  $model = Photo::class;
+
     /**
      * Define the model's default state.
      *
@@ -13,8 +23,13 @@ class PhotoFactory extends Factory
      */
     public function definition()
     {
+        $image = $this->faker->image();
+        $imageFile = new File($image);
         return [
-            //
+            'album_id' => Album::factory(),
+            'title' => $this->faker->sentence,
+            'thumbnail_path' => $path = 'storage/'.Storage::disk('public')->putFile('photos', $imageFile),
+            'thumbnail_url' => config('app.url').'/'.Str::after($path, 'public'),
         ];
     }
 }
