ccb22c30f4bc1fda3db8ae2a115083149f4afc89
anso2410 <simana.annesophie@gmail.com>
2022-05-22 17:06:38 +0200

    Photo.php:add use for slug(spattie) set up options for slug creation and add function to recover the model with its slug

--- app/Models/Photo.php
+++ app/Models/Photo.php
@@ -4,10 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Photo extends Model
 {
-    use HasFactory;
+    use HasFactory, HasSlug;
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug')
+            ->doNotGenerateSlugsOnUpdate();
+    }
 
     public function album()
     {
