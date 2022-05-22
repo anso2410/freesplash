c07776cd396205cc76d60599161008032e24a3e1
anso2410 <simana.annesophie@gmail.com>
2022-05-22 21:58:45 +0200

    SourceFactory:set up rules for faker

--- database/factories/SourceFactory.php
+++ database/factories/SourceFactory.php
@@ -2,19 +2,34 @@
 
 namespace Database\Factories;
 
+use App\Models\Photo;
+use App\Models\Source;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 class SourceFactory extends Factory
 {
+      /**
+     * @var string
+     */
+    protected $model = Source::class;
+
     /**
      * Define the model's default state.
      *
      * @return array
      */
     public function definition()
-    {
+    {   
+        $width = 640; $height = 480;
+        $path = $this->faker->image('public/storage/photos', $width, $height, null, true, true, true, false);
         return [
-            //
+            'photo_id' => Photo::factory(),
+            'path' => $path,
+            'url' => config('app.url').'/'.Str::after($path, 'public/'),
+            'size' => rand(1111, 9999),
+            'width' => $width,
+            'height' => $height,
         ];
     }
 }
