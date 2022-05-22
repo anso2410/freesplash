afe4dc228a8b4c51cd6e5c5f7ac7307afb25eac9
anso2410 <simana.annesophie@gmail.com>
2022-05-22 16:26:22 +0200

    set relation with user and photos for album

--- app/Models/Album.php
+++ app/Models/Album.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Album extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function photos()
+    {
+        return $this->hasMany(Photo::class);
+    }
 }
